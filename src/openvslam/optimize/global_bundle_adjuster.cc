0aeba4ab690988e940f94da0b2d5df2068abe8a8
ymd-stella <world.applepie@gmail.com>
2021-12-29 13:21:03 +0900

    Use marker association in globalBA/localBA

--- src/openvslam/optimize/global_bundle_adjuster.cc
+++ src/openvslam/optimize/global_bundle_adjuster.cc
@@ -1,10 +1,14 @@
 #include "openvslam/data/keyframe.h"
 #include "openvslam/data/landmark.h"
+#include "openvslam/data/marker.h"
 #include "openvslam/data/map_database.h"
+#include "openvslam/marker_model/base.h"
 #include "openvslam/optimize/global_bundle_adjuster.h"
 #include "openvslam/optimize/internal/landmark_vertex_container.h"
+#include "openvslam/optimize/internal/marker_vertex_container.h"
 #include "openvslam/optimize/internal/se3/shot_vertex_container.h"
 #include "openvslam/optimize/internal/se3/reproj_edge_wrapper.h"
+#include "openvslam/optimize/internal/distance_edge.h"
 #include "openvslam/util/converter.h"
 
 #include <g2o/core/solver.h>
@@ -27,12 +31,20 @@ void global_bundle_adjuster::optimize(const unsigned int lead_keyfrm_id_in_globa
 
     const auto keyfrms = map_db_->get_all_keyframes();
     const auto lms = map_db_->get_all_landmarks();
+    const auto markers = map_db_->get_all_markers();
     std::vector<bool> is_optimized_lm(lms.size(), true);
 
     // 2. Construct an optimizer
 
-    auto linear_solver = g2o::make_unique<g2o::LinearSolverCSparse<g2o::BlockSolver_6_3::PoseMatrixType>>();
-    auto block_solver = g2o::make_unique<g2o::BlockSolver_6_3>(std::move(linear_solver));
+    std::unique_ptr<g2o::BlockSolverBase> block_solver;
+    if (markers.size()) {
+        auto linear_solver = g2o::make_unique<g2o::LinearSolverCSparse<g2o::BlockSolverX::PoseMatrixType>>();
+        block_solver = g2o::make_unique<g2o::BlockSolverX>(std::move(linear_solver));
+    }
+    else {
+        auto linear_solver = g2o::make_unique<g2o::LinearSolverCSparse<g2o::BlockSolver_6_3::PoseMatrixType>>();
+        block_solver = g2o::make_unique<g2o::BlockSolver_6_3>(std::move(linear_solver));
+    }
     auto algorithm = new g2o::OptimizationAlgorithmLevenberg(std::move(block_solver));
 
     g2o::SparseOptimizer optimizer;
@@ -128,6 +140,65 @@ void global_bundle_adjuster::optimize(const unsigned int lead_keyfrm_id_in_globa
         }
     }
 
+    // Container of the landmark vertices
+    internal::marker_vertex_container marker_vtx_container(vtx_id_offset, markers.size());
+
+    for (unsigned int marker_idx = 0; marker_idx < markers.size(); ++marker_idx) {
+        auto mkr = markers.at(marker_idx);
+        if (!mkr) {
+            continue;
+        }
+
+        // Convert the corners to the g2o vertex, then set it to the optimizer
+        auto corner_vertices = marker_vtx_container.create_vertices(mkr, false);
+        for (unsigned int corner_idx = 0; corner_idx < corner_vertices.size(); ++corner_idx) {
+            const auto corner_vtx = corner_vertices[corner_idx];
+            optimizer.addVertex(corner_vtx);
+
+            for (const auto& keyfrm : mkr->observations_) {
+                if (!keyfrm) {
+                    continue;
+                }
+                if (keyfrm->will_be_erased()) {
+                    continue;
+                }
+                if (!keyfrm_vtx_container.contain(keyfrm)) {
+                    continue;
+                }
+                const auto keyfrm_vtx = keyfrm_vtx_container.get_vertex(keyfrm);
+                const auto& mkr_2d = keyfrm->markers_2d_.at(mkr->id_);
+                const auto& undist_pt = mkr_2d.undist_corners_.at(corner_idx);
+                const float x_right = -1.0;
+                const float inv_sigma_sq = 1.0;
+                auto reproj_edge_wrap = reproj_edge_wrapper(keyfrm, keyfrm_vtx, nullptr, corner_vtx,
+                                                            0, undist_pt.x, undist_pt.y, x_right,
+                                                            inv_sigma_sq, 0.0, false);
+                reproj_edge_wraps.push_back(reproj_edge_wrap);
+                optimizer.addEdge(reproj_edge_wrap.edge_);
+            }
+        }
+
+        // Add edges for marker shape
+        double informationRatio = 1.0e9;
+        for (unsigned int corner_idx = 0; corner_idx < corner_vertices.size(); ++corner_idx) {
+            const auto dist_edge = new internal::distance_edge();
+            dist_edge->setMeasurement(mkr->marker_model_->width_);
+            dist_edge->setInformation(MatRC_t<1, 1>::Identity() * informationRatio);
+            dist_edge->setVertex(0, corner_vertices[corner_idx]);
+            dist_edge->setVertex(1, corner_vertices[(corner_idx + 1) % corner_vertices.size()]);
+            optimizer.addEdge(dist_edge);
+        }
+        for (unsigned int corner_idx = 0; corner_idx < 2; ++corner_idx) {
+            const auto dist_edge = new internal::distance_edge();
+            const double diagonal_length = std::hypot(mkr->marker_model_->width_, mkr->marker_model_->width_);
+            dist_edge->setMeasurement(diagonal_length);
+            dist_edge->setInformation(MatRC_t<1, 1>::Identity() * informationRatio);
+            dist_edge->setVertex(0, corner_vertices[corner_idx]);
+            dist_edge->setVertex(1, corner_vertices[(corner_idx + 2) % corner_vertices.size()]);
+            optimizer.addEdge(dist_edge);
+        }
+    }
+
     // 5. Perform optimization
 
     optimizer.initializeOptimization();
@@ -178,6 +249,20 @@ void global_bundle_adjuster::optimize(const unsigned int lead_keyfrm_id_in_globa
             lm->pos_w_after_global_BA_ = pos_w;
             lm->loop_BA_identifier_ = lead_keyfrm_id_in_global_BA;
         }
+    }
+
+    for (unsigned int marker_idx = 0; marker_idx < markers.size(); ++marker_idx) {
+        auto mkr = markers.at(marker_idx);
+        if (!mkr) {
+            continue;
+        }
+
+        eigen_alloc_vector<Vec3_t> corner_pos_w;
+        for (unsigned int corner_idx = 0; corner_idx < 4; ++corner_idx) {
+            auto corner_vtx = marker_vtx_container.get_vertex(mkr, corner_idx);
+            corner_pos_w.push_back(corner_vtx->estimate());
+        }
+        mkr->set_corner_pos(corner_pos_w);
     }
 }
 
